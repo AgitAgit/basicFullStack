d2c1f2be854fe81d6311e0eed406f273e39f52bb
sashagriss <sasha.meduza24@gmail.com>
2024-11-05 15:57:07 +0200

    vv

--- Client/dataCenter.js
+++ Client/dataCenter.js
@@ -1,12 +1,9 @@
 const serverAddress = "http://localhost:3000";
 
 export const getJokes = async function () {
-  console.log("test test");
   let jokes = await fetch(`${serverAddress}/api/jokes`);
 
   jokes = await jokes.json();
-
-  // console.log("jokes:", jokes);
 
   return jokes;
 };
