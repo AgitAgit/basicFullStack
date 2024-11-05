6abc34c2af00aef3f663ba75325f0ef3828cc47f
sashagriss <sasha.meduza24@gmail.com>
2024-11-05 13:14:26 +0200

    Your commit message here

---
+++ Client/main.js
@@ -0,0 +1,11 @@
+import { getJokes } from "./dataCenter.js";
+
+const data = await getJokes();
+console.log(data);
+
+const elSetupInput = document.querySelector(".setup");
+const elPunchLineInput = document.querySelector(".punchline");
+
+const elCreateBtn = document.querySelector(".create-btn");
+
+const elForm = document.getElementById("add-form");
